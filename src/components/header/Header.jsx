2b83b6d60a8385959d60dd867dd544de1d729d7e
Lucas Vaz <httpslucasvaz@gmail.com>
2022-12-06 21:59:03 -0300

    feat: add header and style for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
-import './header.css'
+import "./header.css";
 
 export function Header() {
-
-    return(
-        <div id='container'>
-            <nav className='header'>
-                <h1>oi</h1>
-            </nav>
+  return (
+    <div id="container">
+      <nav className="header">
+        <div className="logo-container">
+          <img className="logo" src="/logo.svg" /> <h2>&lt; lucas vaz /&gt;</h2>
         </div>
-    )
+        <div className="logo-container">
+          <h2>facebook</h2>
+          <h2>instagram</h2>
+        </div>
+      </nav>
+    </div>
+  );
 }
