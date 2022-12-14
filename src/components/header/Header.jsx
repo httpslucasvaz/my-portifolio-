40eb32d0d84c01c687b680339fc9624052741e48
Lucas Vaz <httpslucasvaz@gmail.com>
2022-12-13 22:56:30 -0300

    style: style logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
     <div id="container">
       <nav className="header">
         <div className="logo-container">
-          <img className="logo" src="/logo.svg" /> <h2>&lt; lucas vaz /&gt;</h2>
+          <img className="logo" src="/logo.svg" /> <h2>&lt; lucas vaz <span className="color">/</span>&gt;</h2>
         </div>
         <div className="logo-container">
           <button className="cta">
