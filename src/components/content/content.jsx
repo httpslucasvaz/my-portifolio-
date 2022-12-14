826d83c6cce4297bef7af60e7db8dc5f2e303513
Lucas Vaz <httpslucasvaz@gmail.com>
2022-12-13 23:42:43 -0300

    feat: add animation

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,38 +1,56 @@
 import "./content.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import { useEffect } from "react";
 
 export function Content() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <main className="wrapper">
       <div className="container">
-        <img className="illustration" src="/illustration.svg" />
+        <img className="illustration" src="/illustration.svg"  />
 
         <div className="container-text">
           <div className="text">
-            <span className="greet">HELLO, 👋 </span>
-            <span className="text-body">EU SOU O CARA QUE CRIA SITES!</span>
+            <div
+              className="greet"
+              data-aos="fade-left"
+            >
+              HELLO, 👋
+            </div>
+            <span
+              className="text-body"
+              data-aos="fade-left"
+            >
+              EU SOU O CARA QUE CRIA SITES!
+            </span>
 
             <a href="profile.pdf" download>
-            <button className="cssbuttons-io-button">
-              MEU CURRÍCULO
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  height="24"
-                  width="24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-download"
-                >
-                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                  <polyline points="7 10 12 15 17 10"></polyline>
-                  <line x1="12" y1="15" x2="12" y2="3"></line>
-                </svg>
-              </div>
-            </button>
+              <button className="cssbuttons-io-button" data-aos="zoom-out-left">
+                MEU CURRÍCULO
+                <div className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-download"
+                  >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                    <polyline points="7 10 12 15 17 10"></polyline>
+                    <line x1="12" y1="15" x2="12" y2="3"></line>
+                  </svg>
+                </div>
+              </button>
             </a>
           </div>
         </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,25 @@
 import "./header.css";
+import Aos from "aos";
+import 'aos/dist/aos.css'
+
 
 import { SiLinkedin } from "react-icons/si";
 import { SiGithub } from "react-icons/si";
+import { useEffect } from "react";
 
 export function Header() {
+
+  useEffect(() => {
+    Aos.init({duration: 1000})
+  }, [])
+
   return (
     <div id="container">
       <nav className="header">
-        <div className="logo-container">
+        <div className="logo-container" data-aos="fade-right" >
           <img className="logo" src="/logo.svg" /> <h2>&lt; lucas vaz <span className="color">/</span>&gt;</h2>
         </div>
-        <div className="logo-container">
+        <div className="logo-container" data-aos="fade-left">
           <button className="cta">
             <span className="hover-underline-animation"><a href="https://www.linkedin.com/in/lucasvazz/" target='_blank'> linkedin </a> </span>
             <SiLinkedin className="icon" />
